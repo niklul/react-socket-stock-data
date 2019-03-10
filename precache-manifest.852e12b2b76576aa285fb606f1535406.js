a741b6b304bfacf78b014b270ec7c0f9bad66815
niklul <nikhil.allamsetti@gmail.com>
2019-03-10 17:24:24 +0530

    Updates

---
+++ precache-manifest.852e12b2b76576aa285fb606f1535406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c86d25e1def33b2414a",
+    "url": "/react-socket-stock-data/static/js/runtime~main.0942d7f2.js"
+  },
+  {
+    "revision": "3cc11086ad0910c458a9",
+    "url": "/react-socket-stock-data/static/js/main.74d6bf3e.chunk.js"
+  },
+  {
+    "revision": "8d0e370f3484261e6df9",
+    "url": "/react-socket-stock-data/static/js/2.568fdec2.chunk.js"
+  },
+  {
+    "revision": "3cc11086ad0910c458a9",
+    "url": "/react-socket-stock-data/static/css/main.6e117bcf.chunk.css"
+  },
+  {
+    "revision": "0f92e1888e218c68fef0d9f2585ecd79",
+    "url": "/react-socket-stock-data/index.html"
+  }
+];
